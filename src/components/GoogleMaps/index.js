d79226bc1400528e7c44c41f38ddbc8ebaf177e8
Lucas Siqueira <Lucassiqueirafernandes07@gmail.com>
2020-09-19 02:55:35 -0300

    Setting current location

--- src/components/GoogleMaps/index.js
+++ src/components/GoogleMaps/index.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 const GoogleMaps = () => {
+    const [latitude, setLatitude] = useState(0);
+    const [longitude, setLongitude] = useState(0);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition((position) => {
+          setLatitude(position.coords.latitude);
+          setLongitude(position.coords.longitude);
+        });
+    });
+
     return (
         <View style={{flex:1}}>
             <MapView
-                provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+                provider={PROVIDER_GOOGLE}
+                loadingEnabled={true}
                 style={styles.container}
                 region={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
-                    latitudeDelta: 0.015,
+                    latitude: latitude,
+                    longitude: longitude,
+                    latitudeDelta: 0.025,
                     longitudeDelta: 0.0121,
                 }}
             >
